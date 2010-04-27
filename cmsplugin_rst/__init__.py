51043a2e2c2335102549d9a2ef3f0c82eb8c0246
Mark Renton <indexofire@gmail.com>
2010-04-27 14:04:22 +0800

    fix style

--- cmsplugin_rst/__init__.py
+++ cmsplugin_rst/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0,0,1,'alpha', 1)
+VERSION = (0,0,2,'alpha', 1)
 __version__ = '.'.join(map(str, VERSION))
 
 def get_version():
@@ -11,33 +11,3 @@ def get_version():
         if VERSION[3] != 'final':
             version = '%s %s %s' % (version, VERSION[3], VERSION[4])
     return version
-
-# patch settings
-try:
-    from conf import patch_settings
-    from django.conf import settings
-    patch_settings()
-except ImportError:
-    """
-    This exception means that either the application is being built, or is
-    otherwise installed improperly. Both make running patch_settings
-    irrelevant.
-    """
-    pass
-except AttributeError:
-    """
-    Same as Above
-    """
-    pass
-
-"""
-from django.db import models
-from django.contrib import admin
-from cmsplugin_rst.widgets.markitup_widget import ReSTEditor
-from cmsplugin_rst.models import ReSTField
- 
-class ReSTFieldAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.CharField: {'widget': ReSTEditor},
-    }
-"""
